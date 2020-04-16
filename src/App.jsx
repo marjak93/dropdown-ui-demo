36eaf1be94f16777eb313cf9f48bbed9e80e5923
Martin Jakobsen <martinjakobsen93@gmail.com>
2020-04-16 02:22:10 +0200

    Demo description, layout

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 
-import { Dropdown } from "dropdown-ui";
+import { Dropdown } from "@marjak93/dropdown-ui";
 
 import "normalize.css";
 import style from "./style.css";
@@ -15,26 +15,43 @@ const App = () => {
 
   return (
     <div className={style.container}>
-      <h1>Dropdown Demo</h1>
-      <p>Value: {value ? value : "none"}</p>
-      <Dropdown.Select
-        label="Task status"
-        value={value}
-        onChange={handleChange}
-      >
-        <Dropdown.Option value="option1" color="green">
-          Option 1
-        </Dropdown.Option>
-        <Dropdown.Option value="option2" color="blue">
-          Option 2
-        </Dropdown.Option>
-        <Dropdown.Option value="option3" color="orange">
-          Option 3
-        </Dropdown.Option>
-        <Dropdown.Option value="option4" color="red">
-          Denne teksten er såpass lang at den blir brutt.
-        </Dropdown.Option>
-      </Dropdown.Select>
+      <section className={style.description}>
+        <h1>Dropdown Demo</h1>
+        Keyboard interactions (when open):
+        <ul>
+          <li>Arrow up</li>
+          <li>Arrow down</li>
+          <li>Home</li>
+          <li>End</li>
+          <li>Space/Enter</li>
+          <li>Escape</li>
+        </ul>
+      </section>
+
+      <section className={style.component}>
+        <Dropdown.Select
+          label="Task status"
+          value={value}
+          onChange={handleChange}
+        >
+          <Dropdown.Option value="option1" color="green">
+            Option 1
+          </Dropdown.Option>
+          <Dropdown.Option value="option2" color="blue">
+            Option 2
+          </Dropdown.Option>
+          <Dropdown.Option value="option3" color="orange">
+            Option 3
+          </Dropdown.Option>
+          <Dropdown.Option value="option4" color="red">
+            Denne teksten er såpass lang at den blir brutt.
+          </Dropdown.Option>
+        </Dropdown.Select>
+      </section>
+
+      <section className={style.data}>
+        Current value: {value ? value : "none"}
+      </section>
     </div>
   );
 };
